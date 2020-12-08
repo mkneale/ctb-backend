47060d19d523debf133bf76949028a59d7510484
Melissa <mkneale1@hotmail.com>
2020-12-08 17:03:28 +0000

    Totals class created. Spec does not work.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Totals = require('../lib/Totals');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var totals = new Totals;
+totals.getTotalSpendThisPeriod();
 
 module.exports = router;
