17a28de240f8251ef545ea7782cdec638c45470c
Melissa <mkneale1@hotmail.com>
2020-12-10 14:38:31 +0000

    Expenses schema set up

---
+++ spec/expensesSchema.spec.js
@@ -0,0 +1,10 @@
+const { TestScheduler } = require('jest');
+var mongoose = require('mongoose');
+
+require('./mongodb_helper')
+const Expense = require('../models/expense');
+
+describe('ExpenseSchema', function() {
+
+    
+})
