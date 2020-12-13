db6f4f3ed1a941d5a6e2ba82a7c1bebfa9cda4e4
Ben Jones <benwjones76@outlook.com>
2020-12-13 16:02:28 +0000

    added new user routes for totals

--- routes/totals.js
+++ routes/totals.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var totalsController = require('../controllers/totals');
 
 router.get('/', totalsController.Index);
+router.get('/:userId', totalsController.Find);
 
 module.exports = router;
