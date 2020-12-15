3d44bcd4b7a3e324207425b87cef6666f5b83aa1
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-12-15 19:17:52 +0000

    added user controller to find user id by email

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ var usersController = require('../controllers/users');
 router.post('/new', usersController.Create);
 router.post('/login', usersController.Login);
 router.post('/exist', usersController.Exist);
+router.get('/idbyemail/:userName', usersController.Find);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
